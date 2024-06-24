28085cadb56ee6649bae2457434ca134a522e78f
Harounjlassi <jlassi.haroun@esprit.tn>
2024-06-24 11:49:12 +0100

    add quiz

---
+++ lib/QuizBrain.dart
@@ -0,0 +1,13 @@
+class QuizBrain {
+  int quetionNumber = 0;
+  List<Question> QuestionBank = [
+    Question(
+        questionText: 'You can lead a cow down stairs but not up stairs.',
+        questionAnswer: false),
+    Question(
+        questionText:
+            'Approximately one quarter of human bones are in the feet.',
+        questionAnswer: true),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true)
+  ];
+}
